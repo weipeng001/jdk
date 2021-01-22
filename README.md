7c41598bc266a862d1aa70b353af68cebc68d7dc
wuweipeng <592743134@qq.com>
2021-01-22 15:51:58 +0800

    jdk源码搭建指南

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 jdk源码
 * [搭建常见问题解决办法](https://blog.csdn.net/Mr_Programming_Liu/article/details/105158350)
   
+源码版本  1.8.0_112
+  
--- test/HashMapTest.java
+++ test/HashMapTest.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Set;
 
@@ -7,15 +8,20 @@ import java.util.Set;
  */
 public class HashMapTest {
     public static void main(String[] args) {
-        HashMap<String, String> map = new HashMap<>();
-
-        map.put("aaa", "AAA");
-        map.put("bbb", "BBB");
-        map.put("ccc", "CCC");
-
-        Set<String> keys = map.keySet();
-        for (String key : keys) {
-            System.out.println(key + "===>" + map.get(key));
-        }
+        System.out.print(System.getProperty("sun.boot.class.path"));
+//        String property = System.getProperty("sun.boot.class.path");
+//        System.setProperty("sun.boot.class.path","D:\\test\\jdk\\out\\production;" + property);
+//        System.out.println(System.getProperty("sun.boot.class.path"));
+        ArrayList<String> list = new ArrayList<>();
+        list.add("abc");
+//
+//        map.put("aaa", "AAA");
+//        map.put("bbb", "BBB");
+//        map.put("ccc", "CCC");
+//
+//        Set<String> keys = map.keySet();
+//        for (String key : keys) {
+//            System.out.println(key + "===>" + map.get(key));
+//        }
     }
 }
